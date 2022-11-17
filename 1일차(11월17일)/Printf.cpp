4a48e73cec10288fb8a37c4cf504ea27f3fc360f
Kang <118407596+BackendDevKang@users.noreply.github.com>
2022-11-17 14:09:37 +0900

    Update Printf.cpp

--- 1일차(11월17일)/Printf.cpp
+++ 1일차(11월17일)/Printf.cpp
@@ -3,8 +3,8 @@
 int main(void)
 {
 	int add = 3+7; //10
-	printf("3+7= %d\n", add);
-	printf(" %d + %d=%d\n",3,7,3+7);
-	printf("%d x %d = %d\n",10,10,10*100);
+	printf("3+7= %d\n", add); //add가 %d\n에 대입되서 10 출력
+	printf(" %d + %d=%d\n",3,7,3+7); //%d값에 3,7,3+7값을 대입되서 3+3=10이 출력
+	printf("%d x %d = %d\n",10,10,10*10); // $d값에 10,10,10*10을 대입되서 10x10=100d이 출력
 	return 0;
 }
