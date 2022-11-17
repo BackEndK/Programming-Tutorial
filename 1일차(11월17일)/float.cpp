31d7aec318953ee50eb1b02614d37fc1dbdab41d
Kang <118407596+BackendDevKang@users.noreply.github.com>
2022-11-17 14:12:55 +0900

    Update float.cpp

--- 1일차(11월17일)/float.cpp
+++ 1일차(11월17일)/float.cpp
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main(void)
 {
-	float f = 11.1f; 
-	printf("%.2f\n",f); 
+	float f = 11.1f; //float값에는 f를 써야함
+	printf("%.2f\n",f);  %.2는 소수 자리를 정할수있음 float은 %f
 	
-	double d = 1.569;
-	printf("%.2lf\n",d); 
-	
+	double d = 1.569; //double은 f생략 가능
+	printf("%.2lf\n",d); //%.2는 소수 자리를 정할수있음 double은 %lf
+
 	return 0;
 }
